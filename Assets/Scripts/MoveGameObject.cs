dd27be87878d25291181f0e285bff4f597024edd
solst2 <47793395+solst2@users.noreply.github.com>
2020-02-18 15:53:55 +0100

    socks rotation

---
+++ Assets/Scripts/MoveGameObject.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveGameObject : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, 3, 0);
+    }
+
+    void OnMouseDown()
+    {
+       /* float duration = 200000;
+        float t = 0.0f;
+        while (t < duration)
+        {
+            t += Time.deltaTime;
+            transform.Rotate(0, 3, 0);
+        }*/
+    }
+}
